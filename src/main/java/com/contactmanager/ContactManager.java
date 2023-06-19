980b3e2a2855bdc6a2d522db8bba54220eb91d66
T. J. Silva <thiago.silva@imd.ufrn.br>
2023-06-19 15:15:44 -0300

    feat: adiciona validação de contatos
    Adiciona método para validar o primeiro nome, sobrenome e número de telefone do contato antes de adicioná-lo à lista.

--- src/main/java/com/contactmanager/ContactManager.java
+++ src/main/java/com/contactmanager/ContactManager.java
@@ -9,11 +9,18 @@ public class ContactManager {
 
     public void addContact(String firstName, String lastName, String phoneNumber) {
         Contact contact = new Contact(firstName, lastName, phoneNumber);
+        validateContact(contact);
         contactList.put(generateKey(contact), contact);
     }
 
     public Collection<Contact> getAllContacts() {
         return contactList.values();
+    }
+
+    private void validateContact(Contact contact) {
+        contact.validateFirstName();
+        contact.validateLastName();
+        contact.validatePhoneNumber();
     }
 
     private String generateKey(Contact contact) {

da65f2c8f728088ce12c79d0e3d710ac81da4ace
T. J. Silva <thiago.silva@imd.ufrn.br>
2023-06-19 15:57:49 -0300

    feat: adiciona teste de tempo limite na execução da aplicação
    Adiciona um novo teste que verifica se a aplicação é executada com sucesso dentro do tempo limite de 5 segundos. O teste utiliza o método assertTimeout() para verificar o tempo limite, mas ainda não possui uma implementação específica.

--- src/test/java/com/contactmanager/ContactManagerApplicationTests.java
+++ src/test/java/com/contactmanager/ContactManagerApplicationTests.java
@@ -1,13 +1,19 @@
 package com.contactmanager;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.Duration;
+
 @SpringBootTest
 class ContactManagerApplicationTests {
-
 	@Test
+	@DisplayName("Should be able to verify that the app runs successfully within the 5 second timeout")
 	void contextLoads() {
+		Assertions.assertTimeout(Duration.ofSeconds(5), () -> {
+			// TODO: test
+		});
 	}
-
 }

3e7387873f10c70ecf33b47cf9f33d8485f5aa8a
T. J. Silva <thiago.silva@imd.ufrn.br>
2023-06-19 17:00:02 -0300

    feat: adiciona ciclo de vida aos testes de unidade
    Adiciona clico de vida aos testes através dos métodos @BeforeAll, @BeforeEach, @AfterEach e @AfterAll para configurar e limpar os recursos necessários antes e depois dos testes.

--- src/test/java/com/contactmanager/ContactManagerTest.java
+++ src/test/java/com/contactmanager/ContactManagerTest.java
@@ -1,14 +1,32 @@
 package com.contactmanager;
 
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ContactManagerTest {
+    ContactManager contactManager;
+
+    @BeforeAll
+    void setupAll() {
+        System.out.println("All tests have been started");
+    }
+
+    @BeforeEach
+    void setup() {
+        System.out.println("The test has been started");
+        contactManager = new ContactManager();
+    }
+
     @Test
     @DisplayName("Shoud be able to create a contact")
     void should_create_a_contact() {
-        ContactManager contactManager = new ContactManager();
         contactManager.addContact("John", "Doe", "01234567890123");
         Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
         Assertions.assertEquals(1, contactManager.getAllContacts().size());
@@ -24,7 +42,6 @@ class ContactManagerTest {
     @Test
     @DisplayName("Should not be able to create a contact when first name is null")
     void should_not_create_a_contact_when_first_name_is_null() {
-        ContactManager contactManager = new ContactManager();
         Assertions.assertThrows(RuntimeException.class, () -> {
             contactManager.addContact(null, "Doe", "01234567890123");
         });
@@ -33,7 +50,6 @@ class ContactManagerTest {
     @Test
     @DisplayName("Should not be able to create a contact when last name is null")
     void should_not_create_a_contact_when_last_name_is_null() {
-        ContactManager contactManager = new ContactManager();
         Assertions.assertThrows(RuntimeException.class, () -> {
             contactManager.addContact("John", null, "01234567890123");
         });
@@ -42,9 +58,18 @@ class ContactManagerTest {
     @Test
     @DisplayName("Should not be able to create a contact when phone number is null")
     void should_not_create_a_contact_when_phone_number_is_null() {
-        ContactManager contactManager = new ContactManager();
         Assertions.assertThrows(RuntimeException.class, () -> {
             contactManager.addContact("John", "Doe", null);
         });
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("The test has been finished");
+    }
+
+    @AfterAll
+    void tearDownAll() {
+        System.out.println("All tests have been finished");
     }
 }

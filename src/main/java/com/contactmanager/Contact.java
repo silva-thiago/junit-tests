a476e7a8bc1aa9e0539b80be125ab781a43753b2
T. J. Silva <thiago.silva@imd.ufrn.br>
2023-06-19 14:51:56 -0300

    feat: adiciona classe de contato
    Adiciona a classe Contact com os atributos firstName, lastName e phoneNumber, além dos métodos get para cada um desses atributos.

---
+++ src/main/java/com/contactmanager/Contact.java
@@ -0,0 +1,25 @@
+package com.contactmanager;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Contact(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
